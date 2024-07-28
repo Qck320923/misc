30883c32c22edb7255731cdcbac941d048a05f47
L.W.Kevin0wvf <152294811+Qck320923@users.noreply.github.com>
2024-07-28 21:15:53 +0800

    Create list.js

---
+++ javascript/basic functions/list.js
@@ -0,0 +1,16 @@
+//仅作为对Array模块的补充，也可以把这些函数存为Array的方法
+//Just as a supplement to Array module, you can also save these functions as Array methods
+/**
+ * @description 重复数组
+ * @param {any[]} list 数组
+ * @param {number} count 次数
+ * @returns {any[]}
+ */
+repeat(list, count) {
+    if (!list.length || count <= 0) return [];
+    let res = [];
+    for (let i = 1; i < count; i++) {
+        res = res.concat(list);
+    }
+    return res;
+}
